6b15ec45960dfc1d43934db35b77fe4e1c9ab1b2
Your Name <you@example.com>
2020-06-09 01:03:21 +0000

    adfa

---
+++ attach.sh
@@ -0,0 +1,6 @@
+curl --request POST \
+  --url 'https://bitchass.atlassian.net/rest/api/3/issue/TEST-2000000/attachments' \
+  --user 'obkqz@gurumail.xyz:oapymUtsLGgv03GF2rF0A599' \
+  --header 'Accept: application/json' \
+  --header "X-Atlassian-Token: no-check" \
+  -F "file=@zip.zip"
