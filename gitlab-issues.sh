7dbb745332d377d9397ce11b0b83f52edb92b34e
Your Name <you@example.com>
2020-07-14 02:14:36 +0000

    add

--- gitlab-issues.sh
+++ gitlab-issues.sh
@@ -1,1 +1,1 @@
- curl -X POST --header "Private-Token: 7sckh7gUqGJQ9x-Gz_JH" https://gitlab.com/api/v4/projects/18925901/issues?title=bug&description=aaaaaaaaaaaaaaaaaaaaaaaaaaacdfdeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeesafgkjhskgjdhsglshfkashfasklfhalhfuiehafkncaheafhlafhalfehfale
+ curl -X POST --header "Private-Token: 45LgShY8ZCmVvvNAUEyz" --form "file=@$1"   https://gitlab.com/api/v4/projects/19914687/uploads
---
+++ jira-attach.sh
@@ -0,0 +1,11 @@
+
+while true
+do
+
+curl --request POST \
+  --url 'https://bitchass.atlassian.net/rest/api/3/issue/TEST-3397181/attachments' \
+  --user 'obkqz@gurumail.xyz:oapymUtsLGgv03GF2rF0A599' \
+  --header 'X-Atlassian-Token: no-check' \
+  -F "file=@$1"
+
+done
