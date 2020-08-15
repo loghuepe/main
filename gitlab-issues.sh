820ab46467a47c7c8bc63f36d8379074139e969f
Your Name <you@example.com>
2020-08-14 18:28:55 -0700

    kd

--- gitlab-issues.sh
+++ gitlab-issues.sh
@@ -1,1 +1,1 @@
- curl -X POST --header "Private-Token: 45LgShY8ZCmVvvNAUEyz" --form "file=@$1"   https://gitlab.com/api/v4/projects/19914687/uploads
+ curl -X POST --header "Private-Token: USUteYMWH6t6Nv833jNC" --form "file=@$1"   https://gitlab.com/api/v4/projects/20536845/uploads
--- jira-attach.sh
+++ jira-attach.sh
@@ -1,11 +1,7 @@
 
-while true
-do
-
 curl --request POST \
   --url 'https://bitchass.atlassian.net/rest/api/3/issue/TEST-3397181/attachments' \
   --user 'obkqz@gurumail.xyz:oapymUtsLGgv03GF2rF0A599' \
   --header 'X-Atlassian-Token: no-check' \
   -F "file=@$1"
 
-done
