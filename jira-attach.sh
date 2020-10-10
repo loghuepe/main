e2f67dbbf9c96f529c6b0928973a551cdf6a6895
btich <obkqz@gurumail.xyz>
2020-10-10 03:08:47 +0000

    jira-attach.sh edited online with Bitbucket

--- jira-attach.sh
+++ jira-attach.sh
@@ -1,6 +1,6 @@
 
 curl --request POST \
-  --url 'https://bitchass.atlassian.net/rest/api/3/issue/TEST-3397181/attachments' \
+  --url 'https://btich.atlassian.net/rest/api/3/issue/TEST-1/attachments' \
   --user 'obkqz@gurumail.xyz:oapymUtsLGgv03GF2rF0A599' \
   --header 'X-Atlassian-Token: no-check' \
   -F "file=@$1"
