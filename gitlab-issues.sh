e2e95f44f23992cb7b44f760dd85bc6d8636111c
btich <obkqz@gurumail.xyz>
2020-10-12 00:20:52 +0000

    gitlab-issues.sh edited online with Bitbucket

--- gitlab-issues.sh
+++ gitlab-issues.sh
@@ -1,1 +1,1 @@
- curl -X POST --header "Private-Token: USUteYMWH6t6Nv833jNC" --form "file=@$1"   https://gitlab.com/api/v4/projects/20536845/uploads
+ curl -X POST --header "Private-Token: Qd7KAhLdbTYCJb_aSFSv" --form "file=@$1"   https://gitlab.com/api/v4/projects/21714787/uploads
